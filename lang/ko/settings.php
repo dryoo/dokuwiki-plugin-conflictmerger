648f21cac96521f84633e952c34aac257e8816ac
SC Yoo <dryoo@live.com>
2014-10-25 23:41:34 +0900

    Create settings.php

---
+++ lang/ko/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Korean language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     S.C. Yoo <dryoo@live.com>
+ */
+
+$lang['diff3'] = '자동으로 충돌병합을 하기위한 diff3 명령어의 전체경로 (Full path)';
+
